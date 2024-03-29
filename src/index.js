894a799897c5cb108d487dd9f4d41626d05bc81d
Konnichiwaitsnahoko <154089390+Konnichiwaitsnahoko@users.noreply.github.com>
2024-03-29 10:09:16 -0700

    Improve the user experience

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function displayPoem(response) {
+  let generatedPoem = document.querySelector("#generatedPoem");
+  generatedPoem.innerHTML = "";
   new Typewriter("#generatedPoem", {
     strings: response.data.answer,
     autoStart: true,
@@ -9,13 +11,17 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
-  let instructionInput = document.querySelector("#user-instraction");
-  let apiKye = "7e6535efo0b6a8aba82ctdabc0ba3974";
+  let instructionInput = document.querySelector("#user-instruction").value;
+  let apiKey = "7e6535efo0b6a8aba82ctdabc0ba3974";
   let prompt = `User instructions: Generate a poem about ${instructionInput}`;
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a sophisticated 4 line poem in the HTML. Please enter a new line for each line by using <br>. Make sure to follow the user instructions. Example: 'Line 1<br>Line 2<br>Line 3<br>Line 4'";
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4-line poem. Please enter each line separately, using <br> to indicate line breaks. Example: 'Line 1<br>Line 2<br>Line 3<br>Line 4'.";
 
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKye}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#generatedPoem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">  ⌛ Generating a poem about "${instructionInput}" for you...</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
